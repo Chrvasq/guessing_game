3d058b149d2b63804f7e0c307bb54d424994c51e
Chris Vasquez <chrvasq@outlook.com>
2019-05-18 14:40:23 -0700

    Refactor get_user_input function

--- guessing_game.py
+++ guessing_game.py
@@ -9,6 +9,17 @@ Project 1 - Number Guessing Game
 import random
 
 
+def get_user_input():
+    try:
+        user_input = int(input('Pick a number between 1 and 10: '))
+        if user_input not in range(1, 11):
+            raise ValueError
+    except ValueError:
+        print('Your guess should be an integer between 1 and 10. ' +
+            'Please try again!')
+        get_user_input()
+    return user_input
+
 def start_game(high_score=None, score_tracker=[]):
     # Display an intro/welcome message to the player.
     welcome_message = ' Welcome to the Guessing Game! '
@@ -28,32 +39,26 @@ def start_game(high_score=None, score_tracker=[]):
 
     # Continuously prompt the player for a guess.
     while game_state:
-        try:
-            guess = int(input('Pick a number between 1 and 10: '))
-            if guess not in range(1, 11):
-                raise ValueError
-        except ValueError:
-            print('Your guess should be an integer between 1 and 10. ' +
-                'Please try again!')
+        guess = get_user_input()
+
+        if guess > solution:
+            print('It\'s lower!')
+            guess_count += 1
+        elif guess < solution:
+            print('It\'s higher!')
+            guess_count += 1
         else:
-            if guess > solution:
-                print('It\'s lower!')
-                guess_count += 1
-            elif guess < solution:
-                print('It\'s higher!')
-                guess_count += 1
-            else:
-                print('You got it!')
-                guess_count += 1
-                game_state = False
-                score_tracker.append(guess_count)
+            print('You got it!')
+            guess_count += 1
+            game_state = False
+            score_tracker.append(guess_count)
 
     print(f'You guessed in {guess_count} attempts!')
 
     while not game_state:
         try:
             replay = input('Would you like to play again? Y/N: ')
-            if replay not in ['Y', 'y', 'N', 'n']:
+            if replay.lower() not in ['y', 'n']:
                 raise ValueError
         except ValueError:
             print('That\'s not a correct option. Please enter \'Y\' or \'N\'.')
@@ -62,9 +67,9 @@ def start_game(high_score=None, score_tracker=[]):
                 high_score = min(score_tracker)
                 start_game(high_score, score_tracker)
             else:
-                print('The game is now over. Thanks for playing!')
+                print('\n The game is now over. Thanks for playing!\n ')
+                break
 
 
 if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
     start_game()
